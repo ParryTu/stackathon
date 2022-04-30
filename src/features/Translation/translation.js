ee2cd6c3f50efae7db4377b0d9cbd8b891b33aec
Parry <pt987@nyu.edu>
2022-04-30 12:39:09 -0400

    added translate and translated box, debugged speech to text

--- src/features/Translation/translation.js
+++ src/features/Translation/translation.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
-let alltext = "";
 function Box(props) {
   const {
     transcript,
@@ -12,27 +11,63 @@ function Box(props) {
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
+  let [text, setText] = useState(transcript);
+
+  useEffect(() => {
+    setText(text + transcript);
+    console.log(text);
+  }, [listening]);
+
+  useEffect(() => {
+    if (text && text[text.length - 1] != " ") {
+      setText(text + " ");
+    }
+  });
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  function handleOnChange(evt) {
+    setText(evt.target.value);
+    console.log(text);
   }
 
   return (
     <>
       <div>
-        <p>{alltext}</p>
-        <p>Microphone: {listening ? "on" : "off"}</p>
+        <p>Microphone: {listening ? "on, speak now" : "off"}</p>
         <button onClick={SpeechRecognition.startListening}>Start</button>
         <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
         <button
           onClick={() => {
-            alltext = alltext + ". " + transcript;
-            console.log(alltext);
-            return alltext;
+            resetTranscript();
+            text = "";
+          }}
+        >
+          Reset
+        </button>
+        <button
+          onClick={() => {
+            if (transcript) {
+              setText(text + " " + transcript);
+            }
+            console.log(text);
           }}
         >
           Store
         </button>
+        <br></br>
+        <label for="original">Text to translate: </label>
+        <textarea
+          id="original"
+          name="original"
+          rows="5"
+          value={text}
+          onChange={handleOnChange}
+        />
+        <label for="translated">Translated Text: </label>
+        <textarea id="translated" name="translated" rows="5" />
         <p>{transcript}</p>
       </div>
     </>
